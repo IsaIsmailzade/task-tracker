86d9b9d91cb449b70439b22ccbdbbcf1dc4bfcae
Isa <isaismzade@gmail.com>
2025-04-29 17:45:48 +0300

    feat: add Task entity

---
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/model/Task.java
@@ -0,0 +1,37 @@
+package com.isa.tasktrackerbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status", nullable = false)
+    private TaskStatus taskStatus;
+
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
