65934f242c577388802bde36dacf4bbbb810142b
Isa <isaismzade@gmail.com>
2025-05-06 17:18:25 +0300

    feat: add validation annotations to fields

--- task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/dto/AuthRequestDto.java
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/dto/AuthRequestDto.java
@@ -1,9 +1,17 @@
 package com.isa.tasktrackerbackend.security.dto;
 
-import lombok.Value;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-@Value
+@Data
 public class AuthRequestDto {
-    String username;
-    String password;
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email format")
+    private String username;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
 }
