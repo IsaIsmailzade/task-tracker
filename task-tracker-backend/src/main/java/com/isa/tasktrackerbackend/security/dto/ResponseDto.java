fa73aaf904a29a3c8b77110031b824d3631ba125
Isa <isaismzade@gmail.com>
2025-05-06 17:24:24 +0300

    feat: delete second constructor

--- task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/dto/ResponseDto.java
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/dto/ResponseDto.java
@@ -1,9 +1,4 @@
 package com.isa.tasktrackerbackend.security.dto;
 
-import java.util.Map;
-
-public record ResponseDto(String accessToken, String message, Map<String, String> errors) {
-    public ResponseDto(String accessToken, String message) {
-        this(accessToken, message, null);
-    }
+public record ResponseDto(String accessToken, String message) {
 }
