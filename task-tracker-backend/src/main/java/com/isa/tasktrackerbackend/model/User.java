4233064fe1befce947e5ee082a7b088c9a523150
Isa <isaismzade@gmail.com>
2025-06-03 12:46:37 +0300

    fix: remove @Data annotation and add @Getter, @Setter annotations

--- task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/model/User.java
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/model/User.java
@@ -1,10 +1,7 @@
 package com.isa.tasktrackerbackend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -12,10 +9,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User implements UserDetails {
