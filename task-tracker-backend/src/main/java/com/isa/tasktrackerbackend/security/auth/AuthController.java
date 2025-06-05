54f5e1300f8659d185aabc145740bda8c1e75317
Isa <isaismzade@gmail.com>
2025-06-05 20:05:18 +0300

    feat: add AuthController

---
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/auth/AuthController.java
@@ -0,0 +1,34 @@
+package com.isa.tasktrackerbackend.security.auth;
+
+import com.isa.tasktrackerbackend.security.dto.AuthRequestDto;
+import com.isa.tasktrackerbackend.security.dto.RegisterRequestDto;
+import com.isa.tasktrackerbackend.security.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/user")
+    public ResponseEntity<ResponseDto> register(@Validated @RequestBody RegisterRequestDto request) {
+        ResponseDto response = authService.register(request);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + response.accessToken())
+                .body(response);
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<ResponseDto> login(@Validated @RequestBody AuthRequestDto request) {
+        ResponseDto response = authService.authenticate(request);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + response.accessToken())
+                .body(response);
+    }
+}
