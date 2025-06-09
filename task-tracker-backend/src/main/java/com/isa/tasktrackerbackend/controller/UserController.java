6214e9fe469f8030a12b56ec1d41171b76b607c9
Isa <isaismzade@gmail.com>
2025-06-09 18:56:10 +0300

    feat: add UserController

---
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.isa.tasktrackerbackend.controller;
+
+import com.isa.tasktrackerbackend.dto.CurrentUserDto;
+import com.isa.tasktrackerbackend.model.User;
+import com.isa.tasktrackerbackend.service.UserService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@SecurityRequirement(name = "BearerAuth")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public ResponseEntity<CurrentUserDto> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(userService.getCurrentUser(user));
+    }
+}
