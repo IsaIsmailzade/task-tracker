f78f51e15cf71d4bab1d71ee51e4bbe05400a44a
Isa <isaismzade@gmail.com>
2025-06-09 15:10:17 +0300

    fix: change header in authHeader variable from Authentication to HttpHeaders.AUTHORIZATION

--- task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/jwt/JwtFilter.java
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/security/jwt/JwtFilter.java
@@ -1,11 +1,13 @@
 package com.isa.tasktrackerbackend.security.jwt;
 
+import com.isa.tasktrackerbackend.exception.InvalidCredentialsException;
 import com.isa.tasktrackerbackend.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,7 +27,7 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authentication");
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -49,6 +51,8 @@ public class JwtFilter extends OncePerRequestFilter {
                         new WebAuthenticationDetailsSource().buildDetails(request)
                 );
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+            } else {
+                throw new InvalidCredentialsException("Invalid or expired token");
             }
         }
         filterChain.doFilter(request, response);
