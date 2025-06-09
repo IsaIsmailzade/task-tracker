54c26199d4a6cddfb94bd673bbbde7a4d4cef6fe
Isa <isaismzade@gmail.com>
2025-06-09 17:34:02 +0300

    feat: add CurrentUserDto

---
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/dto/CurrentUserDto.java
@@ -0,0 +1,15 @@
+package com.isa.tasktrackerbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CurrentUserDto {
+    Long id;
+    String email;
+}
