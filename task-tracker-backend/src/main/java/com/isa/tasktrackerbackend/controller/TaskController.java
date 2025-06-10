9ba0852663703a5e08ecfd5af79dbf8c99885e52
Isa <isaismzade@gmail.com>
2025-06-10 15:52:14 +0300

    feat: add tasks controller

---
+++ task-tracker-backend/src/main/java/com/isa/tasktrackerbackend/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.isa.tasktrackerbackend.controller;
+
+import com.isa.tasktrackerbackend.dto.CurrentUserTasksDto;
+import com.isa.tasktrackerbackend.model.User;
+import com.isa.tasktrackerbackend.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @GetMapping("/tasks")
+    public ResponseEntity<List<CurrentUserTasksDto>> getCurrentUserTasks(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(taskService.getCurrentUserTasks(user));
+    }
+}
