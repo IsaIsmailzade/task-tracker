a53f2d4cdf7b356e6cd4f77dd0a211dfa5f0a280
Isa <isaismzade@gmail.com>
2025-06-22 19:26:34 +0300

    feat: add kafka consumer config

---
+++ task-tracker-email-sender/src/main/java/com/isa/tasktrackeremailsender/config/KafkaConsumerConfig.java
@@ -0,0 +1,60 @@
+package com.isa.tasktrackeremailsender.config;
+
+import com.isa.tasktrackeremailsender.dto.MessageDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, MessageDto.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<Long, MessageDto> consumerFactory() {
+        JsonDeserializer<MessageDto> jsonDeserializer = new JsonDeserializer<>(MessageDto.class);
+        String pathToDtoPackage = MessageDto.class.getPackageName();
+        jsonDeserializer.addTrustedPackages(pathToDtoPackage);
+        return new DefaultKafkaConsumerFactory<>(
+                consumerConfigs(),
+                new LongDeserializer(),
+                jsonDeserializer
+        );
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<Long,
+            MessageDto>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, MessageDto> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
