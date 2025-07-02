3733deec2915eb9b9e711cf2761902d9d6735cde
Isa <isaismzade@gmail.com>
2025-07-02 18:25:47 +0300

    feat: add user service

---
+++ task-tracker-scheduler/src/main/java/com/isa/scheduler/service/UserService.java
@@ -0,0 +1,18 @@
+package com.isa.scheduler.service;
+
+import com.isa.scheduler.model.User;
+import com.isa.scheduler.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
