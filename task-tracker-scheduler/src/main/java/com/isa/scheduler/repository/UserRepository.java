323435adaf57ee11d233275f32a9ced5eb5e94bf
Isa <isaismzade@gmail.com>
2025-07-02 18:23:43 +0300

    feat: add user repository

---
+++ task-tracker-scheduler/src/main/java/com/isa/scheduler/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.isa.scheduler.repository;
+
+import com.isa.scheduler.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
