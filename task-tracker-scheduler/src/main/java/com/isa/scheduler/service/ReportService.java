a934cd8dc95863d732b8ccc27135d648951891a5
Isa <isaismzade@gmail.com>
2025-07-09 16:27:29 +0300

    feat: add service for sending daily reports

---
+++ task-tracker-scheduler/src/main/java/com/isa/scheduler/service/ReportService.java
@@ -0,0 +1,30 @@
+package com.isa.scheduler.service;
+
+import com.isa.scheduler.dto.MessageDto;
+import com.isa.scheduler.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReportService {
+    private final KafkaProducerService kafkaProducerService;
+    private final MessageService messageService;
+    private final UserService userService;
+
+    //    @Scheduled(fixedRate = 120000) // for method testing
+    @Scheduled(cron = "${scheduler.report-service.cron-new}")
+    public void sendReport() {
+        List<User> users = userService.findAll();
+
+        for (User user : users) {
+            MessageDto messageDto = messageService.createTotalMessage(user);
+            if (messageDto != null) {
+                kafkaProducerService.sendMessage(messageDto);
+            }
+        }
+    }
+}
